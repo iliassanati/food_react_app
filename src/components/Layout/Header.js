b30b75d8557d059e8bbcef4b29cb0675cdfd89fb
iliassanati <iliassanati@gmail.com>
2022-07-18 23:23:32 +0100

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,15 @@
+const Header = (props) => {
+  return (
+    <>
+      <header>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+        <div>
+          <img />
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
