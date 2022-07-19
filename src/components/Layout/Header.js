0edcc74af6c5b4015116f09e31ae69c1c174a034
iliassanati <iliassanati@gmail.com>
2022-07-19 14:45:12 +0100

    adding cart and context

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,17 @@
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
 const Header = (props) => {
   return (
     <>
-      <header>
+      <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
-        <div>
-          <img />
-        </div>
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='a table full of food' />
+      </div>
     </>
   );
 };
